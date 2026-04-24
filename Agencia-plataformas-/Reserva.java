a43823f11697d8eb4d28aebc2c53a6c08a6d012d
JuanVera11 <juanestebanv0612@gmail.com>
2026-04-24 00:24:28 -0500

    en Reserva actualice datos de pago y fechas

--- Agencia-plataformas-/Reserva.java
+++ Agencia-plataformas-/Reserva.java
@@ -1,29 +1,42 @@
-public class Reserva{
+public class Reserva {
 
     int id;
     String usuario;
     String vuelo;
     String hotel;
+    String fechaReserva;
+    double precioTotal;
+    boolean esPagada;
+    String estado; 
 
-    void crear(int id, String usuario, String vuelo, String hotel) {
+    void crear(int id, String usuario, String vuelo, String hotel, String fecha, double precio, boolean pagada, String estado) {
         this.id = id;
         this.usuario = usuario;
         this.vuelo = vuelo;
         this.hotel = hotel;
+        this.fechaReserva = fecha;
+        this.precioTotal = precio;
+        this.esPagada = pagada;
+        this.estado = estado;
     }
 
-        public void mostrarReserva() {
-        System.out.println("ID: " + id);
+    public void mostrarReserva() {
+        System.out.println("ID Reserva: " + id);
+        System.out.println("Cliente: " + usuario);
+        System.out.println("Vuelo Asignado: " + vuelo);
+        System.out.println("Hotel Reservado: " + hotel);
+        System.out.println("Fecha: " + fechaReserva);
+        System.out.println("Estado actual: " + estado);
+    }
+
+    public void imprimirResumen() {
+        System.out.println("\n==================================");
+        System.out.println("        DETALLE DE RESERVA        ");
+        System.out.println("==================================");
+        System.out.println("Reserva No: " + id);
         System.out.println("Usuario: " + usuario);
-        System.out.println("Vuelo: " + vuelo);
-        System.out.println("Hotel: " + hotel);
+        System.out.println("Total a pagar: $" + precioTotal);
+        System.out.println("¿Pago realizado?: " + (esPagada ? "Sí" : "Pendiente"));
+        System.out.println("----------------------------------");
     }
-     public void imprimirResumen() {
-        System.out.println("\n============================");
-        System.out.println("      DETALLE DE RESERVA     ");
-        System.out.println("============================");
-
-
-    }
-
 }
