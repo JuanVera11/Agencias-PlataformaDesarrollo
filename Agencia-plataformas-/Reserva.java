37f274fb93838154a9dc5e6d51e0fe19ede5e185
JuanVera11 <juanestebanv0612@gmail.com>
2026-05-09 15:31:11 -0500

    Juanes modifica la reserva

--- Agencia-plataformas-/Reserva.java
+++ Agencia-plataformas-/Reserva.java
@@ -9,34 +9,34 @@ public class Reserva {
     boolean esPagada;
     String estado; 
 
-     public void crear(int id, String usuario, String vuelo, String hotel, String fecha, double precio, boolean pagada, String estado) {
+    public void crear(int id, String usuario, String vuelo, String hotel, String fechaReserva, double precioTotal, boolean esPagada, String estado) {
         this.id = id;
         this.usuario = usuario;
         this.vuelo = vuelo;
         this.hotel = hotel;
-        this.fechaReserva = fecha;
-        this.precioTotal = precio;
-        this.esPagada = pagada;
+        this.fechaReserva = fechaReserva;
+        this.precioTotal = precioTotal;
+        this.esPagada = esPagada;
         this.estado = estado;
     }
 
     public void mostrarReserva() {
-        System.out.println("ID Reserva: " + id);
-        System.out.println("Cliente: " + usuario);
-        System.out.println("Vuelo Asignado: " + vuelo);
-        System.out.println("Hotel Reservado: " + hotel);
-        System.out.println("Fecha: " + fechaReserva);
-        System.out.println("Estado actual: " + estado);
+        System.out.println("ID Reserva: " + this.id);
+        System.out.println("Cliente: " + this.usuario);
+        System.out.println("Vuelo Asignado: " + this.vuelo);
+        System.out.println("Hotel Reservado: " + this.hotel);
+        System.out.println("Fecha: " + this.fechaReserva);
+        System.out.println("Estado actual: " + this.estado);
     }
 
     public void imprimirResumen() {
         System.out.println("\n==================================");
         System.out.println("        DETALLE DE RESERVA        ");
         System.out.println("==================================");
-        System.out.println("Reserva No: " + id);
-        System.out.println("Usuario: " + usuario);
-        System.out.println("Total a pagar: $" + precioTotal);
-        System.out.println("¿Pago realizado?: " + (esPagada ? "Sí" : "Pendiente"));
+        System.out.println("Reserva No: " + this.id);
+        System.out.println("Usuario: " + this.usuario);
+        System.out.println("Total a pagar: $" + this.precioTotal);
+        System.out.println("¿Pago realizado?: " + (this.esPagada ? "Sí" : "Pendiente"));
         System.out.println("----------------------------------");
     }
 }
